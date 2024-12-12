a81fc060a30c62ee8981b2a3329a2de074447441
Sahilghagg <168530173+Sahilghagg@users.noreply.github.com>
2024-12-12 20:11:29 +0530

    Add files via upload

---
+++ src/quizappliaction1_imgs/JFRame.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sahil
+ */
+public class JFRame extends JFrame {
+
+    public JFRame() {
+    }
+    
+}
